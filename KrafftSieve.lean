1cfc444ed01bc9db949ecf1d8a72f50f82f0bb76
ElNando888 <nando8888@gmail.com>
2026-03-17 00:05:59 +0100

    feat: Add copyright and license header to KrafftSieve.lean.

--- KrafftSieve.lean
+++ KrafftSieve.lean
@@ -1,2 +1,8 @@
+/-
+Copyright (c) 2026 Fernando Portela. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Fernando Portela
+-/
+
 import KrafftSieve.ThirdHarmonic
 import KrafftSieve.Turan_OptimalWeights
